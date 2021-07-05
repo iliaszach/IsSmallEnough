036ff6efb5983201e4fce172e29c81ea53bd2045
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-07-05 12:44:06 +0300

    Add project files.

---
+++ IsSmallEnough/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsSmallEnough
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SmallEnough(new int[] { 101, 45, 75, 105, 99, 107 }, 107));
+        }
+
+        public static bool SmallEnough1(int[] a, int limit)
+        {
+            return Array.TrueForAll(a, x => x <= limit);
+        }
+        public static bool SmallEnough(int[] a, int limit)
+        {
+            bool valid = true;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > limit)
+                {
+                    valid = false;
+                    break;
+                }
+            }
+            return valid;
+        }
+    }   
+
+}
